1fd6ed44a3ff156e89103ae582ec150c654aeae3
Srushti <srushtipatildz@gmail.com>
2026-09-17 14:31:39 +0530

    sec  commit

--- functions.py
+++ functions.py
@@ -7,5 +7,23 @@ def multiply(a,b):
 def div(a,b):
     print("Sum",a/b)
 
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
-add(10,9)
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = int(input("Enter your choice: "))
+match choice:
+    case 1:
+        add(a, b)
+    case 2:
+        sub(a, b)
+    case 3:
+        multiply(a, b)
+    case 4:
+        div(a, b)
+    case _:
+        print("Invalid choice")
