eeb96697edd0bfd3a3e345003784fcefa8ece301
Srushti <srushtipatildz@gmail.com>
2026-09-17 14:21:25 +0530

    First commit

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    print("Sum",a+b)
+def sub(a,b):
+    print("Sum",a-b)
+def multiply(a,b):
+    print("Sum",a*b)                            
+def div(a,b):
+    print("Sum",a/b)
+
+
+add(10,9)
